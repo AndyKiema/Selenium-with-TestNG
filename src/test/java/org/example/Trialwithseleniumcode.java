a7929c076133db7619c164ec517c815b52ce9fdd
Andrew Kiema <andrewkiema94@gmail.com>
2025-05-30 16:45:48 +0300

    Test whether logo is displayed

--- src/test/java/org/example/Trialwithseleniumcode.java
+++ src/test/java/org/example/Trialwithseleniumcode.java
@@ -9,12 +9,26 @@ import java.time.Duration;
 
 public class Trialwithseleniumcode {
     @Test
-    void login(){
+    void login() throws InterruptedException {
         ChromeDriver cd=new ChromeDriver();
         cd.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         cd.get("https://resolve.vote/");
+        cd.manage().window().maximize();
         WebElement pricing=cd.findElement(By.xpath("/html/body/div/header/div/nav/a[2]"));
         pricing.click();
+        Thread.sleep(5000);
+        cd.quit();
+    }
+    @Test
+    void logovisibility(){
+        ChromeDriver ch=new ChromeDriver();
+        ch.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        ch.get("https://resolve.vote/");
+        ch.manage().window().maximize();
+        ch.manage().window().maximize();
+        WebElement logo=ch.findElement(By.xpath("/html/body/div/header/div/div[1]/a/img[1]"));
+        logo.isDisplayed();
+        ch.quit();
     }
 
 }
