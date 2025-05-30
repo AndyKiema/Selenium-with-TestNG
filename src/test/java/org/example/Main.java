3c131d5a4e1cc7ffc7ef72b41387d8ef1939c465
Andrew Kiema <andrewkiema94@gmail.com>
2025-05-30 11:17:21 +0300

    Add before and after method

--- src/test/java/org/example/Main.java
+++ src/test/java/org/example/Main.java
@@ -1,8 +1,25 @@
 package org.example;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class Main {
-
-
-
+    @BeforeMethod
+     void login(){
+         System.out.println("This is login");
+     }
+     @Test(priority =1)
+     public void search(){
+         System.out.println("This is search");
+     }
+     @Test(priority=2)
+     public void advancedsearch(){
+         System.out.println("This is advanced search");
+     }
+     @AfterMethod
+     void logout(){
+         System.out.println("This is logout");
+     }
 }
+//BeforeMethod and AfterMethod runs a number of times before the main test
